298b292e6b3a8487d4f94428973176fddd8e0011
vagfou <foufoulas.evangelos@protonmail.com>
2021-04-04 19:29:55 +0300

    function done

--- script.js
+++ script.js
@@ -5,10 +5,14 @@ generateJoke()
 
 function generateJoke() {
     const config = {
-        headers:{
-            'Accept': 'application/json'
-        }
+        headers: {
+            Accept: 'application/json',
+        },
     }
 
     fetch('https://icanhazdadjoke.com', config)
+        .then((res) => res.json())
+        .then((data) => {
+            jokeEl.innerHTML = data.joke
+        })
 }
